77e57616e5850c16602f5708881d52498c4f0170
Sofi <nsofiia01@gmail.com>
2022-11-22 19:11:15 -0600

    name update

--- Find Cost of Tile to Cover W x L Floor/Program.cs
+++ Find Cost of Tile to Cover W x L Floor/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Find_Cost_of_Tile_to_Cover_W_x_L_Floor;
+namespace Find_Cost_of_Tile_to_Cover_W_x_L_Floor;
     class Program
     {
         const int TILE_PER_HOUR = 20;
@@ -6,32 +6,32 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Hello friend! Let's change your floor.\nWhat is the price you are expecting to pay per 1 tile?");
-        double costSingleTile = (double)Convert.ToDouble(Console.ReadLine());
+        double costSingleTile = Convert.ToDouble(Console.ReadLine());
         double roomArea = 0;
         Console.WriteLine("Got it!\nEnter the number corresponding with the room shape you have:\n" +
             "1. circle\r\n2. suqare or rectangular\r\n3. triangle\r\nType answer here and press enter:"); ;
-        double roomShape = (double)Convert.ToDouble(Console.ReadLine());
+        double roomShape = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Enter your measurements:");
         if (roomShape == 1)
         {
             Console.WriteLine("Radius ");
-            double radius = (double)Convert.ToDouble(Console.ReadLine());
+            double radius = Convert.ToDouble(Console.ReadLine());
             roomArea = Math.PI * radius * radius;
         }
         if (roomShape == 2)
         {
             Console.WriteLine("What is width?");
-            double width = (double)Convert.ToDouble(Console.ReadLine());
+            double width = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("What is length?");
-            double length = (double)Convert.ToDouble(Console.ReadLine());
+            double length = Convert.ToDouble(Console.ReadLine());
             roomArea = width * length;
         }
         if (roomShape == 3)
         {
             Console.WriteLine("What is base length?");
-            double triangleBase = (double)Convert.ToDouble(Console.ReadLine());
+            double triangleBase = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("What is height side length?");
-            double triangleHeight = (double)Convert.ToDouble(Console.ReadLine());
+            double triangleHeight = Convert.ToDouble(Console.ReadLine());
             roomArea = 0.5 * (triangleBase * triangleHeight);
         }
 
@@ -44,4 +44,3 @@
         Console.ReadKey(true);
     }
 }
-
